9f5c8d0138f883cb76f8c07e4852e152b5e0ccd3
essennejaye <sjeffers02@gmail.com>
2020-12-24 21:13:17 -0700

    Updates

---
+++ precache-manifest.8ab7ebef44e4c75f1344d3610d8ce637.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e920273b9840c0c573d7fd9ee45251d6",
+    "url": "/portfolio2/index.html"
+  },
+  {
+    "revision": "02272f0c7152a77033b2",
+    "url": "/portfolio2/static/css/main.c12eb794.chunk.css"
+  },
+  {
+    "revision": "8defe125b268769a835d",
+    "url": "/portfolio2/static/js/2.e22141cc.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolio2/static/js/2.e22141cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02272f0c7152a77033b2",
+    "url": "/portfolio2/static/js/main.2b16a8ea.chunk.js"
+  },
+  {
+    "revision": "4b059261683b5be75625",
+    "url": "/portfolio2/static/js/runtime-main.1ee5a9c2.js"
+  },
+  {
+    "revision": "ea9675e08887cd57fb2e9283f246f64f",
+    "url": "/portfolio2/static/media/Orange-Background-Wallpaper-HD-16464.ea9675e0.png"
+  },
+  {
+    "revision": "0805a3453ad6dd54f6c7fc6e8f2c543c",
+    "url": "/portfolio2/static/media/ProfilePic.0805a345.jpg"
+  },
+  {
+    "revision": "bae872e028d03b71cd325265d155a3d0",
+    "url": "/portfolio2/static/media/bootstrap-logo.bae872e0.png"
+  },
+  {
+    "revision": "b19a870070d6641470f9e80734a011e3",
+    "url": "/portfolio2/static/media/github_icon.b19a8700.png"
+  },
+  {
+    "revision": "d7ee9fab282e98e7d3c9d513e1a7d6d9",
+    "url": "/portfolio2/static/media/html-5-logo.d7ee9fab.png"
+  },
+  {
+    "revision": "cea96a5059e56a9c03bd8fd3f1262480",
+    "url": "/portfolio2/static/media/linkedin_icon.cea96a50.png"
+  },
+  {
+    "revision": "5dc6d17f12a1e248bdf12736d0376a03",
+    "url": "/portfolio2/static/media/my-schtick-kick.5dc6d17f.png"
+  },
+  {
+    "revision": "99fbe7dadb74099d182a90e2db845ce6",
+    "url": "/portfolio2/static/media/mydigilib.99fbe7da.png"
+  },
+  {
+    "revision": "6ca02c9b7069582ce35cf477289274c6",
+    "url": "/portfolio2/static/media/responsive-web-design.6ca02c9b.png"
+  },
+  {
+    "revision": "afe9466b71982d547d38f064b02f3f83",
+    "url": "/portfolio2/static/media/rv-roadtrip.afe9466b.png"
+  },
+  {
+    "revision": "e31c1624fd0b7aa866dfea6ab6a7644e",
+    "url": "/portfolio2/static/media/weather-dashboard.e31c1624.png"
+  },
+  {
+    "revision": "88dcc59d571cf45a95cf636b79b0bc5c",
+    "url": "/portfolio2/static/media/web-dev-op-ed.88dcc59d.png"
+  },
+  {
+    "revision": "f0d7e81d6313afb660fba13df29dfdec",
+    "url": "/portfolio2/static/media/webdev_ss1.f0d7e81d.png"
+  },
+  {
+    "revision": "bffaf1608ce0bbd62aa45c289d0cdeac",
+    "url": "/portfolio2/static/media/webdev_ss2.bffaf160.png"
+  },
+  {
+    "revision": "09458d921b7b7d3977a734cfd65c6e96",
+    "url": "/portfolio2/static/media/webdev_ss3.09458d92.png"
+  },
+  {
+    "revision": "b9f273909fa305cc2280a6c326a05e2f",
+    "url": "/portfolio2/static/media/workday-scheduler.b9f27390.png"
+  }
+]);
